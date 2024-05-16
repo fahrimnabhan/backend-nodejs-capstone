fbcca3fa33130599de9b60fed8c4e929b9136c00
fahrimnabhan <62969514+fahrimnabhan@users.noreply.github.com>
2024-05-16 17:26:19 +0900

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -5,23 +5,22 @@ const connectToDatabase = require('../models/db')
 const logger = require('../logger')
 
 // Define the upload directory path
-const directoryPath = 'public/images';
+const directoryPath = 'public/images'
 
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, directoryPath); // Specify the upload directory
+    cb(null, directoryPath) // Specify the upload directory
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original file name
-  },
-});
+    cb(null, file.originalname) // Use the original file name
+  }
+})
 
-const upload = multer({ storage: storage });
-
+const upload = multer({ storage: storage })
 // Get all secondChanceItems
 router.get('/', async (req, res, next) => {
-    logger.info('/ called');
+    logger.info('/ called')
     try {
         const db = await connectToDatabase();
         const collection = db.collection("secondChanceItems");
